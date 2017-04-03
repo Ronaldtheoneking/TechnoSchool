ab57409c5fef86c93ec1b44b7e8b3ce3ef7526b8
ErickReyes <reyes_erick96@hotmail.com>
2017-04-03 12:18:58 -0600

    Cadena de conexion y modulo de conexion

---
+++ Proyecto de Aministracion de Software/Conexion.vb
@@ -0,0 +1,5 @@
+﻿Imports System.Data.SqlClient
+
+Module Conexion
+    Public cn As New SqlConnection("Data Source=.\SQLEXPRESS;Initial Catalog=ProyectoAdministracion;Integrated Security=True")
+End Module
